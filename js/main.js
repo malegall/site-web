4545234c61dd3c0cfabc9d786257fe9d68f4dba9
MLG <mlegall29asb@gmail.com>
2023-02-22 17:09:45 +0100

    ajout animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var typed = new Typed(".lg", {
+    strings: ["Mathis", "\"MLG\""],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1500,
+    loop: true
+})
+
 const menuHamburger = document.querySelector(".menu")
 const navLinks = document.querySelector(".links")
 
